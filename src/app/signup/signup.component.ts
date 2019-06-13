ffeb2ef0479bff3ca1be043e4da63cdd0a5ccbdc
kanathippack <pack_2541@hotmail.com>
2019-06-13 20:38:29 +0700

    update login/register#1

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,29 +7,29 @@ import { ReactiveFormsModule, FormGroup, FormControl, FormBuilder, Validators } 
  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-userForm: FormGroup;
-emailSignup: string;
- passwordSignup: string;
-constructor(private fb: FormBuilder, private auth: AuthService) {
-    auth.getCurrentLoggedIn();
-}
+    userForm: FormGroup;
+    emailSignup: string;
+    passwordSignup: string;
+
+constructor(private fb: FormBuilder, private auth: AuthService) { }
+
 ngOnInit() {
- this.buildForm();
+    this.buildForm();
  }
 buildForm(): void {
- this.userForm = new FormGroup({
- emailSignup: new FormControl('', [
- Validators.required,
- Validators.email
- ]),
- passwordSignup: new FormControl('', [
- Validators.pattern('^(?=.*[0–9])(?=.*[a-zA-Z])([a-zA-Z0–9]+)$'),
- Validators.minLength(6),
- Validators.maxLength(25)
- ])
- });
+    this.userForm = new FormGroup({
+    emailSignup: new FormControl('', [
+    Validators.required,
+    Validators.email
+    ]),
+    passwordSignup: new FormControl('', [
+    Validators.pattern('^(?=.*[0–9])(?=.*[a-zA-Z])([a-zA-Z0–9]+)$'),
+    Validators.minLength(6),
+    Validators.maxLength(25)
+    ])
+    });
  }
 signup(): void {
- this.auth.emailSignUp(this.userForm.value.emailSignup, this.userForm.value.passwordSignup)
+    this.auth.emailSignUp(this.userForm.value.emailSignup, this.userForm.value.passwordSignup)
  }
 }
